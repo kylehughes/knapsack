523f72b292349e0149dca2ca64a3cb4d393ea539
Kyle Hughes <me@kylehugh.es>
2015-01-04 21:30:19 -0800

    Added ocean themes for fish (configured) and iTerm (available)

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -9,8 +9,8 @@
 # Appearance
 ############
 
-# Color scheme: Base16 Default
-eval sh $HOME/.config/fish/colors/base16-default.dark.sh
+# Color scheme: Base16 Ocean
+eval sh $HOME/.config/fish/colors/base16_ocean.dark.sh
 
 # Colorize maven output
-eval sh $HOME/.config/fish/scripts/maven-antsy-color/mvn
+alias mvn="$HOME/.config/fish/scripts/maven-antsy-color/mvn"
