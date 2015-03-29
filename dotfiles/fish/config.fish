700b61731d2e7805e68c2a5c45cadc023473992e
Kyle Hughes <me@kylehugh.es>
2015-03-28 23:07:57 -0700

    Cleaned up iTerm and fish colors

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,16 +1,14 @@
 ###############
-# Instantiation
+# Setup
 ###############
 
 # Source local profile
 . $HOME/.config/fish/profile.fish
 
-############
+###############
 # Appearance
-############
+###############
 
 # Color scheme: Base16 Ocean
 eval sh $HOME/.config/fish/colors/base16_ocean.dark.sh
 
-# Colorize maven output
-eval sh $HOME/.config/fish/scripts/maven-antsy-color/mvn
